15ce7835bb52cfeaf7d5c164d890200a797543d3
jdavidson018 <jdavidson018@gmail.com>
2019-11-19 12:44:12 -0600

    Updates

---
+++ precache-manifest.7e6795a712100554423a4ed96f652158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00887a04071a63c8ce275bb5b709056",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "b1463f55ce617828c280",
+    "url": "/secret-santa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fbac51549512a9e100f",
+    "url": "/secret-santa/static/js/2.6b2e4680.chunk.js"
+  },
+  {
+    "revision": "b1463f55ce617828c280",
+    "url": "/secret-santa/static/js/main.acd69837.chunk.js"
+  },
+  {
+    "revision": "a20138bd90b480768272",
+    "url": "/secret-santa/static/js/runtime-main.ec9bbe7b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/secret-santa/static/media/logo.25bf045c.svg"
+  }
+]);
